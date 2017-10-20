9de9d18abf7bafb58b79f6a633a70554052d9fe7
Jeannie <jflaherty@utexas.edu>
2017-10-20 11:13:44 -0500

    moved everything

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+//dev.js
+
+module.exports = {
+  googleClientID:
+    '325029729669-taf8ujo2eqptj7drrp6fnjom3cmltld1.apps.googleusercontent.com',
+  googleClientSecret: '2lZw0hrUzYNFkv41vmV2B-ko',
+  mongoURI: 'mongodb://hdocapstone:password@ds125555.mlab.com:25555/hdo-res',
+  cookieKey: 'kladjlfkajldiuoqiwemjadlnkjnljkeruou'
+};
