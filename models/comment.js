3a70a4f96a3a7e784385964d0833a3d5e4737f93
Jeannie <jflaherty@utexas.edu>
2017-10-23 11:01:33 -0500

    fsadfsdfvadsfsd

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 //or const {Schema}=mongoose; -- these are the same
 
 const commentSchema = new Schema({
-  comment: textarea
+  comment: String
 });
 
 module.exports = mongoose.model('comment', commentSchema);
