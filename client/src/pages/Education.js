226c0208e37040279970d27809a98c8fc3230eaf
Jeannie <jflaherty@utexas.edu>
2017-10-24 12:43:53 -0500

    revision of education Page

--- client/src/pages/Education.js
+++ client/src/pages/Education.js
@@ -15,35 +15,35 @@ const Education = () => {
             Orthotic and Prosthetic practitioners are in a unique field that
             blend patient care with design and fabrication of devices for the
             physically challenged. They are similar in many ways to other
-            healthcare providers: They see patients to evaluate their physical
-            orthopedic needs, recommend a tratment pan for improved physical
-            mobility, and in coordination with physicians, physical therapists
-            or other health care professionals provide an orthopedic device to
-            benefit the patient. They either build a custom device or they will
-            have access to prefabricated devices to do this.
+            healthcare providers in that they see patients to evaluate their
+            physical orthopedic needs, recommend a treatment plan for improved
+            physical mobility, and in coordination with physicians, physical
+            therapists or other health care professionals the orthotist will
+            design, fabricate, and fit custom-made orthopedic braces, also known
+            as "orthoses". The orthotist may fit pre-fabricated devices and
+            provide related patient care. A prosthetist designs, fabricates, and
+            fits custom-made artificial limbs, or "prostheses," and provides
+            related patient care. provide an orthopedic device to benefit the
+            patient.
           </p>
 
           <p>
-            An orthotist designs, fabricates, and fits custom-made orthopedic
-            braces, or "orthoses," and fits pre-fabricated devices, and provides
-            related patient care. A prosthetist designs, fabricates, and fits
-            custom-made artificial limbs, or "prostheses," and provides related
-            patient care. O&P practitioners are integral members of the
-            healthcare and rehabilitation team. Practitioners evaluate patients,
-            formulate treatment plans, and provide follow-up care and continued
-            patient management that ensures the overall welfare of their
-            patients. Their unique expertise in patient assessment, design, and
-            materials offers patients increased mobility and independence.
-            Practitioners work in settings such as patient care facilities,
-            hospitals, VA facilities, rehabilitation facilities, long-term care
-            facilities, and in patients' homes. Steps to a career as an O&P
-            practitioner See a list of O&P practitioner education programs.
-            After completion of the master's degree program, you must complete a
-            residency in orthotics, prosthetics, or both. To learn more about
-            residencies, visit the NCOPE website. To become certified,
-            candidates must pass certification exams in orthotics, prosthetics,
-            or both from the American Board for Certification in Orthotics,
-            Prosthetics and Pedorthics (ABC), the Board of
+            Orthotists and Prosthetic practitioners are integral members of the
+            healthcare and rehabilitation team. These practitioners evaluate
+            patients, formulate treatment plans, and provide follow-up care and
+            continued patient management that ensures the overall welfare of
+            their patients. Their unique expertise in patient assessment,
+            design, and materials offers patients increased mobility and
+            independence. Practitioners work in settings such as patient care
+            facilities, hospitals, VA facilities, rehabilitation facilities,
+            long-term care facilities, and in patients' homes. Steps to a career
+            as an O&P practitioner See a list of O&P practitioner education
+            programs. After completion of the master's degree program, you must
+            complete a residency in orthotics, prosthetics, or both. To learn
+            more about residencies, visit the NCOPE website. To become
+            certified, candidates must pass certification exams in orthotics,
+            prosthetics, or both from the American Board for Certification in
+            Orthotics, Prosthetics and Pedorthics (ABC), the Board of
             Certification/Accreditation, International (BOC), or both. To learn
             more about the requirements for certification, please visit the ABC
             website or the BOC website.
@@ -53,7 +53,7 @@ const Education = () => {
             <div>
               <img
                 src="/css/img/MastersPrograms.png"
-                alt="current o&p Masters Programs"
+                alt="O&P Masters Programs-2017"
                 width="650"
               />
             </div>
