b3a26cefbd762af7008af7f2d2c69a92d35ea6c7
Jeannie <jflaherty@utexas.edu>
2017-11-14 17:02:15 -0600

    logo

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Grid, Nav, NavItem } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
+
+// ReactDOM.render(
+//   <SocialIcon url="http://twitter.com/jaketrent" />,
+//   document.body
+// );
 
 function Footer(/*props*/) {
   return (
@@ -7,14 +14,26 @@ function Footer(/*props*/) {
       <div className="row">
         <Grid>
           <Nav justified>
-            <NavItem eventKey={1}>
-              {/* <img src="/css/img/logo.png" alt="" width="100" /> */}
+            <NavItem eventKey={1} href="https://github.com/jf-utex">
+              <SocialIcon
+                url="https://github.com/jf-utex"
+                color="#A6327A"
+                style={{ height: 25, width: 25 }}
+              />
             </NavItem>
-            <NavItem eventKey={2}>
-              {/* <img src="/css/img/logo.png" alt="" width="100" /> */}
+            <NavItem
+              eventKey={2}
+              href="https://www.linkedin.com/in/jeannie-flaherty-369427122/"
+            >
+              <SocialIcon
+                url="https://www.linkedin.com/in/jeannie-flaherty-369427122/"
+                color="#A6327A"
+                style={{ height: 25, width: 25 }}
+              />
             </NavItem>
+
             <NavItem eventKey={3} href="https://jf-utex.github.io">
-              <img src="/css/img/logo.png" alt="" width="100" />
+              <img src="/css/img/bearlogo.png" alt="" width="50" />
             </NavItem>
           </Nav>
         </Grid>
