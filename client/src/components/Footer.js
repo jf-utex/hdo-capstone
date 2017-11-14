e7db96127c27e313cf4badddc6615047102f49ca
Jeannie <jflaherty@utexas.edu>
2017-11-14 17:18:17 -0600

    jhgkg,jhgbl

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { Grid, Nav, NavItem } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 
-// ReactDOM.render(
-//   <SocialIcon url="http://twitter.com/jaketrent" />,
-//   document.body
-// );
-
 function Footer(/*props*/) {
   return (
     <footer>
