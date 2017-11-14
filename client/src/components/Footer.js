8e6e938903b4c00a8a0e6f528f00fc5300fa3da6
Jeannie <jflaherty@utexas.edu>
2017-11-14 17:13:15 -0600

    social icons killed heroku build

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Grid, Nav, NavItem } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
-import { SocialIcon } from 'react-social-icons';
 
 // ReactDOM.render(
 //   <SocialIcon url="http://twitter.com/jaketrent" />,
@@ -14,23 +13,11 @@ function Footer(/*props*/) {
       <div className="row">
         <Grid>
           <Nav justified>
-            <NavItem eventKey={1} href="https://github.com/jf-utex">
-              <SocialIcon
-                url="https://github.com/jf-utex"
-                color="#A6327A"
-                style={{ height: 25, width: 25 }}
-              />
-            </NavItem>
+            <NavItem eventKey={1} href="https://github.com/jf-utex" />
             <NavItem
               eventKey={2}
               href="https://www.linkedin.com/in/jeannie-flaherty-369427122/"
-            >
-              <SocialIcon
-                url="https://www.linkedin.com/in/jeannie-flaherty-369427122/"
-                color="#A6327A"
-                style={{ height: 25, width: 25 }}
-              />
-            </NavItem>
+            />
 
             <NavItem eventKey={3} href="https://jf-utex.github.io">
               <img src="/css/img/bearlogo.png" alt="" width="50" />
